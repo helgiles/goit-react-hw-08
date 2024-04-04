24a46943ce46d3d6efb3e0e791d5076c74624057
Олег Слюсар <eternityonly1@gmail.com>
2024-04-04 11:46:14 +0300

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,66 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// const axiosInstance = axios.create({
-//   baseURL: 'https://connections-api.herokuapp.com',
-// });
-
-// export const fetchContacts = createAsyncThunk(
-//   'contacts/fetchContacts',
-//   async (_, thunkAPI) => {
-//     try {
-//       const response = await axiosInstance.get('/contacts');
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async (newContact, thunkAPI) => {
-//     try {
-//       const response = await axiosInstance.post('/contacts', newContact);
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const deleteContact = createAsyncThunk(
-//   'contacts/deleteContact',
-//   async (contactId, thunkAPI) => {
-//     try {
-//       const response = await axiosInstance.delete(`/contacts/${contactId}`);
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const editContact = createAsyncThunk(
-//   'contacts/editContact',
-//   async (updateContact, thunkAPI) => {
-//     try {
-//       const response = await axiosInstance.patch(
-//         `/contacts/${updateContact.id}`,
-//         {
-//           name: updateContact.name,
-//           number: updateContact.number,
-//         }
-//       );
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
